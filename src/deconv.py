24a67e724898bbae08d93d3d4f9708260d72e193
Joel Bader <joel.bader@jhu.edu>
2012-03-06 13:13:44 -0500

    Adding logging

--- src/deconv.py
+++ src/deconv.py
@@ -4,5 +4,17 @@ Deconvolution for antibody pools
 joel.bader@jhu.edu
 """
 
+import logging
+logger = logging.getLogger(name='deconv')
+logger.setLevel(logging.DEBUG)
+
+def get_data_dir():
+    data_dir = '/Users/joel/Dropbox/Pooled data and individual retests_12511/Pools'
+    logger.info('data_dir %s', data_dir)
+    return(data_dir)
+
+def main():
+    data_dir = get_data_dir()
+
 if __name__ == '__main__':
     main()
